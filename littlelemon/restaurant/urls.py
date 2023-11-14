145581fa801a4e9e1ed7ae1eef9ba58d38051bc5
Mitch <118388516+avidcyclist@users.noreply.github.com>
2023-11-14 15:06:14 -0600

    Add files via upload

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin 
 from django.urls import path 
 from . import views
-from .views import MenuItemView, SingleMenuItemView, index
+from .views import MenuItemView, SingleMenuItemView
+#import obtain_auth_token view
+from rest_framework.authtoken.views import obtain_auth_token
   
 urlpatterns = [ 
    path('menu/items/', MenuItemView.as_view(), name='menu-list-create'),
    path('menu/items/<int:pk>/', SingleMenuItemView.as_view(), name='menu-detail'),
    path('', views.index, name='index'),
+   path('api-token-auth/', obtain_auth_token),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -4,6 +4,8 @@ from rest_framework import generics, viewsets
 from rest_framework.decorators import api_view
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 
 def sayHello(request):
@@ -23,3 +25,4 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
